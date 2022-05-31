b33cb9c7da9ca7a1aba088e1d57fdb5ac06480a2
SuperSonic <supersonic@livemail.tw>
2022-05-31 23:05:25 +0800

    Update for https

--- src/execute.js
+++ src/execute.js
@@ -3,10 +3,17 @@ const http = require('http');
 const https = require('https');
 
 function _general(app, callback) {
-    app.listen(process.env.HTTP_PORT, process.env.HTTP_HOSTNAME, callback);
+    const httpServer = http.createServer(app);
+    httpServer.listen(parseInt(process.env.HTTP_PORT), process.env.HTTP_HOSTNAME);
+    callback();
 }
 
 function _secure(app, callback) {
+    const credentials = {
+        key: fs.readFileSync(process.env.HTTPS_KEY_PATH),
+        cert: fs.readFileSync(process.env.HTTPS_CERT_PATH)
+    };
+
     if (process.env.HTTPS_REDIRECT === 'yes') {
         app.use((req, res, next) => {
             if (req.protocol === 'http') {
@@ -16,24 +23,18 @@ function _secure(app, callback) {
         });
     }
 
-    const credentials = {
-        key: fs.readFileSync(process.env.HTTPS_KEY_PATH),
-        cert: fs.readFileSync(process.env.HTTPS_CERT_PATH)
-    };
-
-    const server = http.createServer(app);
     const httpsServer = https.createServer(credentials, app);
-
-    server.listen(parseInt(process.env.HTTP_PORT), process.env.HTTP_HOSTNAME);
     httpsServer.listen(parseInt(process.env.HTTPS_PORT), process.env.HTTP_HOSTNAME);
-
     callback();
 }
 
 module.exports = function (app, callback) {
-    if (process.env.HTTPS !== 'yes') {
+    if (process.env.HTTPS === 'both') {
         _general(app, callback);
-    } else {
         _secure(app, callback);
+    } else if (process.env.HTTPS === 'only') {
+        _secure(app, callback);
+    } else {
+        _general(app, callback);
     }
 }
