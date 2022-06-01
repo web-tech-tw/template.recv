6239a6765de9d6f7e071c85237b94ecc7d1844aa
SuperSonic <supersonic@livemail.tw>
2022-06-01 14:59:31 +0800

    Update sara_token.js

--- src/utils/sara_token.js
+++ src/utils/sara_token.js
@@ -4,10 +4,27 @@
 // Import jsonwebtoken
 const jwt = require('jsonwebtoken');
 
-// Validate Function
+// Import SHA256 Generator
+const {sha256} = require('js-sha256');
+
+// Define general_validate_options Generator
+const general_validate_options = (metadata) => ({
+    algorithms: ["HS256"],
+    audience: process.env.SARA_AUDIENCE,
+    complete: true,
+    issuer: process.env.SARA_ISSUER || sha256(metadata.ctx.jwt_secret)
+});
+
+// Validate Function (Auth)
 function validateAuthToken(ctx, token) {
     try {
-        return jwt.verify(token, ctx.jwt_secret, null, null);
+        const validate_options = general_validate_options({ctx});
+        const data = jwt.verify(token, ctx.jwt_secret, validate_options, null);
+        if (data?.header?.sara?.version !== 1 || data?.header?.sara?.type !== "auth") {
+            console.error("invalid_sara_code_token");
+            return false;
+        }
+        return data.payload;
     } catch (e) {
         console.error(e);
         return false;
