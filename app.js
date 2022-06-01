e818aaced0470797521ace50b3281f826b542648
SuperSonic <supersonic@livemail.tw>
2022-06-01 19:21:06 +0800

    Add handler for /robots.txt

--- app.js
+++ app.js
@@ -29,6 +29,8 @@ app.get('/', (_, res) => {
     res.redirect(StatusCodes.MOVED_PERMANENTLY, process.env.WEBSITE_URL);
 });
 
+app.get('/robots.txt', (_, res) => res.type('txt').send("User-agent: *\nDisallow: /"));
+
 app.get('/ip', (req, res) => {
     res.send({ip_address: util.ip_address(req)});
 });
