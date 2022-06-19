4a698188e69a6e2b068b46e93ae6d14524861b0c
SuperSonic <supersonic@livemail.tw>
2022-06-19 12:59:46 +0800

    Improve middlewares, auth and access

--- src/middlewares/access.js
+++ src/middlewares/access.js
@@ -1,6 +1,7 @@
 "use strict";
 // Check the role for the request required,
 // and interrupt if the requirement is not satisfied.
+// (for Sara only)
 
 // Import StatusCodes
 const {StatusCodes} = require("http-status-codes");
@@ -10,11 +11,22 @@ const {StatusCodes} = require("http-status-codes");
 // set as string, it will find the role whether satisfied
 // set as null, will check the user whether login only
 module.exports = (role) => (req, res, next) => {
-    const userRoles = req?.authenticated?.user?.roles;
+    // Check auth exists
+    if (!req.auth) {
+        throw Error(
+            "req.auth is unset, " +
+            "please add middleware \"auth\" before \"access\".",
+        );
+    }
+    // Accept SARA only
+    if (req.auth.method !== "SARA") return;
+    // Read roles from metadata
+    const userRoles = req.auth.metadata?.user?.roles;
     if (!(userRoles && Array.isArray(userRoles))) {
         res.sendStatus(StatusCodes.UNAUTHORIZED);
         return;
     }
+    // Check permission
     if (role && !userRoles.includes(role)) {
         res.sendStatus(StatusCodes.FORBIDDEN);
         return;
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -10,7 +10,7 @@ const {StatusCodes} = require("http-status-codes");
 // Import authMethods
 const authMethods = {
     "SARA": async (ctx, req, _) =>
-        require("../utils/sara_token").validateAuthToken(ctx, req.auth_secret),
+        require("../utils/sara_token").validateAuthToken(ctx, req.auth.secret),
 };
 
 // Export (function)
@@ -25,16 +25,20 @@ module.exports = (ctx) => function(req, res, next) {
         next();
         return;
     }
-    req.auth_method = params[0];
-    req.auth_secret = params[1];
-    if (!(req.auth_method in authMethods)) {
+    req.auth = {
+        id: null,
+        metadata: null,
+        method: params[0],
+        secret: params[1],
+    };
+    if (!(req.auth.method in authMethods)) {
         next();
         return;
     }
-    authMethods[req.auth_method](ctx, req, res)
+    authMethods[req.auth.method](ctx, req, res)
         .then((result) => {
-            if (!req.authenticated) {
-                req.authenticated = result;
+            if (!req.auth.metadata) {
+                req.auth.metadata = result;
             }
             next();
         })
