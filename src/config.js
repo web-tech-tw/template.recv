a2112dd59ac4b88b9a3f4b085a0ec67644b24b6a
SuperSonic <supersonic@livemail.tw>
2023-01-19 11:17:19 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,14 +1,19 @@
 "use strict";
 
+// Import modules
+const {join: pathJoin} = require("path");
+const {existsSync} = require("fs");
+
 /**
  * Load configs from system environment variables.
  */
 function runLoader() {
-    const {join: pathJoin} = require("path");
-    const {existsSync} = require("fs");
-
     const dotenvPath = pathJoin(__dirname, "..", ".env");
-    if (!existsSync(dotenvPath) && process.env.APP_CONFIGURED !== "1") {
+
+    const isDotEnvFileExists = existsSync(dotenvPath);
+    const isCustomDefined = process.env.APP_CONFIGURED === "1";
+
+    if (!isDotEnvFileExists && !isCustomDefined) {
         console.error(
             "No '.env' file detected in app root.",
             "If you're not using dotenv file,",
@@ -40,9 +45,29 @@ function getEnvironmentOverview() {
     };
 }
 
+/**
+ * Shortcut to get config value.
+ * @param {string} key the key
+ * @return {string} the value
+ */
+function get(key) {
+    return process.env[key];
+}
+
+/**
+ * The get the vale from config with fallback.
+ * @param {string} key the key
+ * @param {string} fallback the fallback value
+ * @return {string} the excepted value
+ */
+function getFallback(key, fallback) {
+    return get(key) || fallback;
+}
 
 module.exports = {
     runLoader,
     isProduction,
     getEnvironmentOverview,
+    get,
+    getFallback,
 };
