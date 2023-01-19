74a706c503ebc9c2bfb9e359e9187585d0e845e3
SuperSonic <supersonic@livemail.tw>
2023-01-19 22:46:03 +0800

    Update new_secret.js

--- new_secret.js
+++ new_secret.js
@@ -2,20 +2,29 @@
 
 // Import modules
 const fs = require("node:fs");
-const crypto = require("node:crypto");
+const {
+    createHash,
+    randomInt,
+    randomBytes,
+} = require("node:crypto");
 
 // Import constant
 const constant = require("./src/init/const");
 
 // Define variables
-const length = crypto.randomInt(2048, 1000000);
-const bytes = crypto.randomBytes(length);
+const length = randomInt(2048, 1000000);
+const bytes = randomBytes(length);
 const secret = Buffer.from(bytes).toString("base64");
+
+// Define hash function - SHA256
+const sha256hex = (data) =>
+    createHash("sha256").update(data).digest("hex");
 
 // Write the secret file
 try {
     fs.writeFileSync(constant.SECRET_FILENAME, secret);
     console.info(
+        `Secret Hash (sha256): ${sha256hex(secret)}`,
         `The secret has been saved into "${constant.SECRET_FILENAME}".`,
     );
 } catch (e) {
